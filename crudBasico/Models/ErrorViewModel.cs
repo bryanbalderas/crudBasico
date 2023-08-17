172d41816f80096a67832f101c08596c13c1b891
bryanbalderas <bbalderas@gmail.com>
2023-08-16 19:43:23 -0600

    Agregar archivos de proyecto.

---
+++ crudBasico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace crudBasico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
